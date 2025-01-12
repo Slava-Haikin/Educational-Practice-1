4f130a102ff156d714d72f46ef477a1cc66a831f
Slava Haikin <vyachowski@gmail.com>
2025-01-12 23:30:35 +0800

    feat: add app structure template

---
+++ Task_1-Currency_Converter/app.js
@@ -0,0 +1,10 @@
+const app = () => {
+  console.log('App is working, wow!')
+
+  //TODO - APP STRUCTURE
+  // 0. Get basic user currency
+  // 1. Fetch currencies data
+  // 2. Add handlers
+}
+
+app();
