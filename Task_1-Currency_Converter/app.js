6074041d99c8556a0322f7685f02858ca7752dc2
Slava Haikin <vyachowski@gmail.com>
2025-01-13 10:05:46 +0800

    feat: add initial data fetching

--- Task_1-Currency_Converter/app.js
+++ Task_1-Currency_Converter/app.js
@@ -1,10 +1,54 @@
-const app = () => {
-  console.log('App is working, wow!')
+const SUCCESS_CODE_API = 'success'
+const DEFAULT_CURRENCY = 'USD'
 
+const getUserIp = () => {
+  return fetch('https://api.ipify.org?format=json')
+    .then((response) => response.json())
+    .then(({ ip }) => ip)
+}
+
+const fetchUserCurrencyCode = async (ip) => {
+  const userInfoQueryLink = `http://ip-api.com/json/${ip}?fields=status,currency`
+
+  return fetch(userInfoQueryLink)
+    .then((response) => response.json())
+    .then((result) => result)
+}
+
+const fetchRatesData = async (currencyCode) => {
+  const ratesQueryLink = `https://open.er-api.com/v6/latest/${currencyCode}`
+
+  return fetch(ratesQueryLink)
+    .then((response) => response.json())
+    .then((result) => result)
+}
+
+const fetchInitialData = async () => {
+  let userCurrency = DEFAULT_CURRENCY
+
+  const userIp = await getUserIp()
+  const { status, currency } = await fetchUserCurrencyCode(userIp)
+
+  if (status === SUCCESS_CODE_API) {
+    userCurrency = currency
+  }
+
+  const { result, rates } = await fetchRatesData(userCurrency)
+
+  if (result !== SUCCESS_CODE_API) {
+    alert('Something went wrong, please check your internet connection and reload the page.')
+    return
+  }
+
+  return {userCurrency, rates}
+}
+
+const app = async () => {
+  const { userCurrency, rates} = await fetchInitialData();
+
+  console.log(userCurrency, rates)
   //TODO - APP STRUCTURE
-  // 0. Get basic user currency
-  // 1. Fetch currencies data
   // 2. Add handlers
 }
 
-app();
+app()
